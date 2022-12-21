c34c78c04a7c660b115b049bba1451f7253d3a9b
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-21 18:52:12 +0530

    Setup

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { nanoid } from 'nanoid'
+  const id = nanoid()
+  console.log(id)
+</script>
+
+<template>
+  <input style="width:100%" type="text" v-bind-once="{ id, name: id, value: id }" />
+  <label v-bind-once="{ for: id }" />
+</template>
